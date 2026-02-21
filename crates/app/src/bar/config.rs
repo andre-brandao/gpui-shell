085bc84c8c9ec2049d08003e151891d1caef7154
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2026-02-20 21:11:05 -0300

    test: docs workflow

--- crates/app/src/bar/config.rs
+++ crates/app/src/bar/config.rs
@@ -41,7 +41,7 @@ pub struct BarConfig {
     pub center: Vec<String>,
     /// End section widgets (right for horizontal, bottom for vertical).
     pub end: Vec<String>,
-    /// Per-module configuration.
+
     pub modules: ModulesConfig,
 }
 
